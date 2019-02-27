d05b7a65c3497f9193df921288ab7736eb693e63
Alex18mai <alex18mai@gmail.com>
2019-02-27 12:23:05 +0200

    functie vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Mode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Mode.java
@@ -9,7 +9,17 @@ import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+import static org.firstinspires.ftc.teamcode.MineralPosition.LEFT;
+import static org.firstinspires.ftc.teamcode.MineralPosition.MIDDLE;
+import static org.firstinspires.ftc.teamcode.MineralPosition.RIGHT;
 
 public abstract class Autonomous_Mode extends LinearOpMode {
 
@@ -34,6 +44,15 @@ public abstract class Autonomous_Mode extends LinearOpMode {
     //constante
     protected final int tics_per_cm = 67;
     protected static double TOLERANCE = 0.0001;
+
+    //vuforia stuff
+    private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
+    private static final String VUFORIA_KEY = "AYlEu/7/////AAABmXB1kirNm0vlrZa4DCCmkis6ZNJkEkHGNYjIfoKWcK+yxnJOhuC4Lw3B63L+Y5vrSoTsr1mEe6bvGcMR8Hg+v1Z1Cih0IrBRHdIfrrg6lfa723ft/unZOKgck3ftCj8gWuiM89d+A4smkenUI5P/HXMKMGKCk4xxv5of9YNSX8r4KFO8lD+bqYgnP+GVXzD/TwQo7Dqer3bf0HVbOqP6j6HREHAZdP6Idg/JwyRG8LSdC6ekTwogxCWsuWiaUhuC8uAQ4r/ZfJykZpXYCxhdcLwMM4OaUXkUAPuUenzxlL8MXkwOhsDfqiQNEfSB00BodWKq28EC6cc+Vsko8r9PreeU6jCYR4d84VK8uBFLGaJx";
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+    private boolean CameraOrientation = false; //false -> back ; true -> front
 
     //functii abstrcte
     protected abstract void runOperations();
@@ -88,6 +107,21 @@ public abstract class Autonomous_Mode extends LinearOpMode {
         MotorBR.setPower(0);
         MotorBL.setPower(0);
 
+        //vuforia
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        if (CameraOrientation){
+            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        }
+        else{
+            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
+        }
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
+
         //setare color
         //color.enableLed(true);
 
@@ -96,6 +130,77 @@ public abstract class Autonomous_Mode extends LinearOpMode {
         //while (gyro.isCalibrating()){
         //   idle();
         //}
+    }
+
+    //************
+    //VUFORIA
+    //************
+
+    protected MineralPosition Position(){
+        MineralPosition ret = null;
+        tfod.activate();
+        while (opModeIsActive()) {
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                //telemetry.addData("# Object Detected", updatedRecognitions.size());
+                if (updatedRecognitions.size() >= 2) {
+                    String label1 = "";
+                    String label2 = "";
+                    int top1 = -1; //top de la tel cand e vertical , deci left pt landscape
+                    int top2 = -1;
+                    int left1 = 100000; //top de la tel cand e vertical , deci buttom pt landscape
+                    int left2 = 100000;
+                    for (Recognition recognition : updatedRecognitions) {
+                        //telemetry.addData("label" , recognition.getLabel());
+                        //telemetry.addData("left" , (int)recognition.getLeft());
+                        //telemetry.addData("top" , (int)recognition.getTop());
+                        //telemetry.addData("" , "");
+                        if (left1 > (int) recognition.getLeft()){
+                            left2 = left1;
+                            top2 = top1;
+                            label2 = label1;
+
+                            left1 = (int) recognition.getLeft();
+                            top1 = (int) recognition.getTop();
+                            label1 = recognition.getLabel();
+                        }
+                        else if (left2 > (int) recognition.getLeft()){
+                            left2 = (int) recognition.getLeft();
+                            top2 = (int) recognition.getTop();
+                            label2 = recognition.getLabel();
+                        }
+                    }
+                    if (top1 != -1 && top2 != -1) {
+                        if (label1.equals(LABEL_GOLD_MINERAL) || label2.equals(LABEL_GOLD_MINERAL)){
+                            if (label1.equals(LABEL_GOLD_MINERAL)){
+                                if (top1 < top2){
+                                    ret = LEFT;
+                                }
+                                else{
+                                    ret = MIDDLE;
+                                }
+                            }
+                            else {
+                                if (top2 < top1){
+                                    ret = LEFT;
+                                }
+                                else {
+                                    ret = MIDDLE;
+                                }
+                            }
+                        }
+                        else{
+                            ret = RIGHT;
+                        }
+                        break;
+                    }
+                    telemetry.addData("position : " , ret);
+                }
+                telemetry.update();
+            }
+        }
+        tfod.shutdown();
+        return ret;
     }
 
     //************
